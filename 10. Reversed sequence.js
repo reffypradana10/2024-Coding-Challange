1302032319bb11334839ab2f4d2ebe4a93e24219
reffypradana10 <reffypradana10@gmail.com>
2024-02-03 22:21:31 +0700

    reverse sequence

---
+++ 10. Reversed sequence.js
@@ -0,0 +1,16 @@
+// const reverseSeq = (n) => {
+//   const result = [];
+//   for (let i = n; i >= 1; i--) {
+//     //   console.log(i);
+//     result.push(i);
+//   }
+
+//   return result;
+// };
+
+const reverseSeq = (n) =>
+  Array(n)
+    .fill()
+    .map((el, i) => n - i);
+
+console.log(reverseSeq(5));
