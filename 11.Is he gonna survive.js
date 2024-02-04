3ffa34d216fd5d7f79cb17468d8f766d251ce4da
reffypradana10 <reffypradana10@gmail.com>
2024-02-05 00:55:01 +0700

    is he gonna survive

---
+++ 11.Is he gonna survive.js
@@ -0,0 +1,13 @@
+// function hero(bullets, dragons) {
+//   if (bullets / 2 >= dragons) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function hero(bullets, dragons) {
+//   return bullets / 2 >= dragons;
+// }
+
+const hero = (bullets, dragons) => bullets / 2 >= dragons;
