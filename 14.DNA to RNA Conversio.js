8cd5df59370c3db5516e14c30ef89e98a4eccf13
reffypradana10 <reffypradana10@gmail.com>
2024-02-14 09:08:15 +0700

    DNA to RNA convert

---
+++ 14.DNA to RNA Conversio.js
@@ -0,0 +1,22 @@
+// function DNAtoRNA(dna) {
+//   let rna = "";
+// //   for (let i = 0; i < dna.length; i++) {
+//     // if (dna[i] === "T") {
+//     //   rna += "U";
+//     // } else {
+//     //   rna += dna[i];
+//     // }
+// //     rna += dna[i] === 'T' ? 'U' :dna[i]
+// //   }
+// //   return rna;
+// }
+
+// DNAtoRNA = (dna) =>
+//   dna
+//     .split("")
+//     .map((el) => (el === "T" ? "U" : el))
+//     .join("");
+
+// const DNAtoRNA = (dna) => dna.replace(/T/g, "U");
+const DNAtoRNA = (dna) => dna.replaceAll("T", "U");
+console.log(DNAtoRNA("TTTT"));
