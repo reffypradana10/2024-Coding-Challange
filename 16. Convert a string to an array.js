a3cc54c8ed1ab17d263647d959ccfc081c1f53e0
reffypradana10 <reffypradana10@gmail.com>
2024-02-17 20:04:09 +0700

    Write a function to split a string and convert it into an array of words.

---
+++ 16. Convert a string to an array.js
@@ -0,0 +1,14 @@
+// WPU Codding Callange
+// Day 16
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/javascript
+// Write a function to split a string and convert it into an array of words.
+
+// function stringToArray(string){
+
+// 	return string.split(' ')
+
+// }
+
+const stringToArray = (string) => string.split(" ");
+
+console.log(stringToArray("Robin Singh"));
