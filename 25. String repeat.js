7675254c5e1bb612b90a77a5c3f6735d0ef001c8
reffypradana10 <reffypradana10@gmail.com>
2024-03-14 04:49:46 +0700

    Day 25, https://www.codewars.com/kata/57a0e5c372292dd76d000d7e

---
+++ 25. String repeat.js
@@ -0,0 +1,19 @@
+// WPU Codding Callange
+// String repeat
+// Day 25
+// https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Cara bar bar
+function repeatStr(n, s) {
+  let kata = [];
+  for (let i = 0; i < n; i++) {
+    kata += s;
+  }
+  return kata;
+}
+// best clever, engga kepikiran pakai method repeat
+const repeatStr = (n, s) => s.repeat(n);
+console.log(repeatStr(3, "*"));
+console.log(repeatStr(5, "#"));
+console.log(repeatStr(2, "ha "));
