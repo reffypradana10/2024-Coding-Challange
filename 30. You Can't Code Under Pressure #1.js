dc1ac41f3fdb9bfc4b9b1d61d140612c1a130665
reffypradana10 <reffypradana10@gmail.com>
2024-03-14 08:04:52 +0700

    Day 30, You Can't Code Under Pressure #1

---
+++ 30. You Can't Code Under Pressure #1.js
@@ -0,0 +1,15 @@
+// WPU Codding Callange
+// You Can't Code Under Pressure #1
+// Day 30
+// https://www.codewars.com/kata/53ee5429ba190077850011d4
+// Code as fast as you can! You need to double the integer and return it.
+
+// barbar code
+// function doubleInteger(i) {
+//     let b = i*2
+//     return b;
+//   }
+
+doubleInteger = (nilai) => nilai * 2;
+
+console.log(doubleInteger(2));
