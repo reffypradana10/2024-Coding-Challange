acd40f802c907667b462c5f573570f4406e322de
reffypradana10 <reffypradana10@gmail.com>
2024-03-15 17:03:33 +0700

    Day 32, Opposite number

---
+++ 32.Opposite number.js
@@ -0,0 +1,18 @@
+// WPU Codding Callange
+// Opposite number
+// Day 32
+// Kata 8
+// https://www.codewars.com/kata/56dec885c54a926dcd001095
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// barbar code
+// function opposite(number) {
+//   return -number
+// }
+
+// Simple Model
+const opposite = (n) => -n;
+
+console.log(opposite(1));
+console.log(opposite(0));
+console.log(opposite(4.25));
